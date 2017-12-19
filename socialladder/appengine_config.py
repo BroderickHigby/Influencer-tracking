c1334ffe6d16beb9b6d77ea1863d57f5a134d483
Rafael Pivato <rafael@pivato.info>
2017-12-19 14:21:07 -0800

    Organize lib call

--- socialladder/appengine_config.py
+++ socialladder/appengine_config.py
@@ -1,9 +1,11 @@
-def webapp_add_wsgi_middleware(app):
-	from google.appengine.ext.appstats import recording
-	app = recording.appstats_wsgi_middleware(app)
-	return app
-
 from lib import add_lib_path
 add_lib_path()
+
+
+def webapp_add_wsgi_middleware(app):
+    from google.appengine.ext.appstats import recording
+    app = recording.appstats_wsgi_middleware(app)
+    return app
+
 
 appstats_SHELL_OK = True
--- socialladder/lib/__init__.py
+++ socialladder/lib/__init__.py
@@ -1,8 +1,9 @@
 import os
 import sys
 
+
 def add_lib_path():
-	lib_directory = os.path.dirname(os.path.abspath(__file__))
-	if lib_directory not in sys.path:
-		sys.path.insert(0, lib_directory)
-	sys.path.append('/srv/socialladder')
+    lib_directory = os.path.dirname(os.path.abspath(__file__))
+    if lib_directory not in sys.path:
+        sys.path.insert(0, lib_directory)
+        sys.path.append('/srv/socialladder')
