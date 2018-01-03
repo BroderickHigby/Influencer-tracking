323c5fd1f61e5a1cc0b653ce32eca5aa877f68c0
Rafael Pivato <rpivato@gmail.com>
2018-01-03 05:58:34 -0800

    ReactJS store added

--- sapie-fe/src/App.js
+++ sapie-fe/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Store } from 'react-rebind';
 import { BrowserRouter, Route, Redirect, Switch, Link } from 'react-router-dom';
 
 import Body from './layout/Body';
@@ -7,20 +8,35 @@ import Header from './layout/Header';
 import Home from './page/Home';
 import Search from './page/Search';
 
+import actions from './actions';
+
 class App extends Component {
+
+    get initialStore() {
+        return {
+            resources: {
+                concept: [],
+                part: [],
+                rule: []
+            }
+        };
+    }
+
     render() {
         return (
             <BrowserRouter>
-              <Body>
-                <Header>
+              <Store actions={actions} initial={this.initialStore}>
+                <Body>
+                  <Header>
                   <Link to="/app/home">Sapie Space</Link>
-                </Header>
-                <Switch>
-                  <Route path="/app/home" component={Home} />
-                  <Route path="/app/search" component={Search} />
-                  <Redirect to="/app/home" />
-                </Switch>
-              </Body>
+                  </Header>
+                  <Switch>
+                    <Route path="/app/home" component={Home} />
+                    <Route path="/app/search" component={Search} />
+                    <Redirect to="/app/home" />
+                  </Switch>
+                </Body>
+              </Store>
             </BrowserRouter>
         );
     }
---
+++ sapie-fe/src/actions.js
@@ -0,0 +1,7 @@
+function search(scope, term) {
+    console.debug('asked to search', scope, term);
+}
+
+export default {
+    search
+};
