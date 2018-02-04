6a5bb5f91d64aace6a494c3e25766700a3aff80a
thonmakerformvp <mkeane@ucsd.edu>
2018-02-04 12:05:13 -0800

    removed ugly print statements from twitter scraper.

--- sapie-wc/twittergod/twittergod/twitter_scraper.py
+++ sapie-wc/twittergod/twittergod/twitter_scraper.py
@@ -31,10 +31,4 @@ class TwitterScraper:
                 diff = timeNow - item_returned.created_at
                 post_age_in_minutes = (diff.days * 24 * 60) + (diff.seconds/60)
                 now = datetime.datetime.utcnow()
-                print(item_returned)
-                print("********")
-                print(item_returned.author)
-                print("$$$$$$$")
-                print(item_returned.user)
-                print("##########")
                 self.writerPosts.writerow([item_returned.author.name.encode('utf-8'), topic, item_returned.created_at, post_age_in_minutes, item_returned.in_reply_to_user_id_str, item_returned.is_quote_status, item_returned.in_reply_to_screen_name, item_returned.in_reply_to_status_id, item_returned.in_reply_to_status_id_str, item_returned.user.profile_image_url.encode('utf-8'), item_returned.user.id, item_returned.user.id_str.encode('utf-8'), item_returned.user.followers_count, item_returned.user.has_extended_profile, item_returned.user.profile_text_color.encode('utf-8'), item_returned.contributors, item_returned.source_url.encode('utf-8'), item_returned.source.encode('utf-8'), item_returned.author.followers_count, item_returned.text.encode('utf-8'), item_returned.retweet_count, item_returned.favorite_count, item_returned.id, item_returned.id_str, item_returned.lang, now, item_returned.author.location.encode('utf-8'), item_returned.author.screen_name.encode('utf-8'), item_returned.author.description.encode('utf-8')])
