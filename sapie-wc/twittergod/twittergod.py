c4096dcf0c48eca1fdd396d16b8be0130a67cb9a
thonmakerformvp <mkeane@ucsd.edu>
2018-02-04 11:52:57 -0800

    Added twitter scraper

---
+++ sapie-wc/twittergod/twittergod.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+root_path = os.path.abspath('.')
+sys.path.append(root_path + '/' + 'twittergod')
+
+from twitter_scraper import *
+twitter_voyager = TwitterScraper()
+twitter_voyager.scrape_by_topics(topics=["superbowl", "america"])
