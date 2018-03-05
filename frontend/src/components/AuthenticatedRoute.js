97a4cea72b6ece8d8a8952ee6b85d1bb61a33ee0
Nick Bavafa <nbavafa@gmail.com>
2018-03-05 13:42:07 -0800

    Changed to make it signup

--- frontend/src/components/AuthenticatedRoute.js
+++ frontend/src/components/AuthenticatedRoute.js
@@ -8,6 +8,6 @@ export default ({ component: C, props: cProps, ...rest }) =>
       cProps.isAuthenticated
         ? <C {...props} {...cProps} />
         : <Redirect
-            to={'/app/login'}
+            to={'/app/signup'}
           />}
   />;
