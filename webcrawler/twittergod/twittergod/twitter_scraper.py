9f6e58ecf5c821234adaeb1b3b343ed40c2f51be
phara23 <instaparktechnologies@gmail.com>
2018-03-07 15:27:02 -0800

    allows gathering data on demographics of a users followers on twitter

--- webcrawler/twittergod/twittergod/twitter_scraper.py
+++ webcrawler/twittergod/twittergod/twitter_scraper.py
@@ -2,6 +2,8 @@ import tweepy
 from tweepy import OAuthHandler
 import csv
 import datetime
+import time
+import boto3
 from constants import *
 
 class TwitterScraper:
@@ -37,3 +39,68 @@ class TwitterScraper:
             return user.description, user.favourites_count, user.followers_count, user.friends_count, user.id_str, user.screen_name
         except:
             return '', '', '', '', '', ''
+    def analyze_demographics_of_followers(self, username):
+        following_usernames = TwitterScraper.get_followers_info(username, self.api)
+        TwitterScraper.extract_followers_data(following_usernames)
+
+    @staticmethod
+    def get_followers_info(username, api):
+        users = tweepy.Cursor(api.followers, screen_name=username).items()
+        print(users)
+        following_users = []
+        while True:
+            try:
+                user = next(users)
+            except tweepy.TweepError:
+                time.sleep(60*15)
+                user = next(users)
+            except StopIteration:
+                break
+            #print ("@" + user.screen_name)
+            following_users.append(user)
+            if len(following_users) > 330:
+                break
+        return following_users
+
+    @staticmethod
+    def extract_followers_data(followers):
+        print("in extract_followers_data")
+        locations = {}
+        cited_interests = {}
+        languages = {}
+
+        #analyze pictures(profile_image_url) for:
+        #genders = {}
+        #ages = {}
+
+        comprehend = boto3.client(service_name='comprehend', region_name='us-west-2')
+
+        for follower in followers:
+            if follower.description != '':
+                if follower.lang == 'en' or follower.lang == 'es':
+                    key_phrases = comprehend.detect_key_phrases(Text=follower.description, LanguageCode=follower.lang)
+                    for key_phrase in key_phrases['KeyPhrases']:
+                        normalized_key_phrase = key_phrase['Text'].lower()
+                        if normalized_key_phrase not in cited_interests:
+                            cited_interests[normalized_key_phrase] = 1
+                        else:
+                            cited_interests[normalized_key_phrase] = cited_interests[normalized_key_phrase] + 1
+
+            if follower.location not in locations:
+                locations[follower.location] = 1
+            else:
+                locations[follower.location] = locations[follower.location] + 1
+
+            if follower.lang not in languages:
+                languages[follower.lang] = 1
+            else:
+                languages[follower.lang] = languages[follower.lang] + 1
+        print('popular locations:')
+        print(locations)
+        print("\n")
+        print('popular languages:')
+        print(languages)
+        print("\n")
+        print('popular nouns:')
+        cited_interests2 = {k: v for k, v in cited_interests.items() if v > 1}
+        print(cited_interests2)
