84cd15587030ab1c22c2f0f75656bbf8fa6f38b1
raymondarevalo <arevalo.raymond@gmail.com>
2018-03-06 18:11:10 -0800

    Made signup page responsive

--- frontend/src/page/Signup.js
+++ frontend/src/page/Signup.js
@@ -19,6 +19,11 @@ import Flexbox from 'flexbox-react';
 import logo from "../halfLogo.png";
 import Login from "./Login.js";
 
+import sapielogo from "../sapielogo.png";
+import searchIcon from "../search.svg";
+import connectionsIcon from "../connections.svg";
+import multipleIcon from "../multiple.svg";
+
 
 const submitButtonStyle = {
   backgroundColor: '#711AAC',
@@ -27,6 +32,11 @@ const submitButtonStyle = {
   padding: '10px 10px',
   border: '0',
   fontSize: '1em'
+}
+
+const iconStyle = {
+  height: '10px',
+  marginRight: '10px'
 }
 
 
@@ -136,178 +146,181 @@ export default class Signup extends Component {
     });
 
     return new Promise((resolve, reject) =>
-      userPool.signUp(username, password, attributeList, null, (err, result) => {
-        if (err) {
-          reject(err);
-          return;
-        }
+    userPool.signUp(username, password, attributeList, null, (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-        resolve(result.user);
-      })
-    );
-  }
-
-  confirm(user, confirmationCode) {
-    return new Promise((resolve, reject) =>
-      user.confirmRegistration(confirmationCode, true, function(err, result) {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(result);
-      })
-    );
-  }
-
-  authenticate(user, username, password) {
-    const authenticationData = {
-      Username: username,
-      Password: password
-    };
-    const authenticationDetails = new AuthenticationDetails(authenticationData);
-
-    return new Promise((resolve, reject) =>
-      user.authenticateUser(authenticationDetails, {
-        onSuccess: result => resolve(),
-        onFailure: err => reject(err)
-      })
-    );
-  }
-
-  resendConfirmCode() {
-    return new Promise((resolve, reject) =>
-      this.state.newUser.resendConfirmationCode(function(err, result) {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(result);
-      })
-    );
+      resolve(result.user);
+    })
+  );
 }
 
-  renderConfirmationForm() {
-    return (
-      <form onSubmit={this.handleConfirmationSubmit}>
-        <FormGroup controlId="confirmationCode" bsSize="large">
-          <ControlLabel>Confirmation Code</ControlLabel>
-          <FormControl
-            autoFocus
-            type="tel"
-            value={this.state.confirmationCode}
-            onChange={this.handleChange}
-          />
-          <HelpBlock>Please check your email for the code.</HelpBlock>
-        </FormGroup>
-        <LoaderButton
-          block
-          bsSize="large"
-          disabled={!this.validateConfirmationForm()}
-          type="submit"
-          isLoading={this.state.isLoading}
-          text="Verify"
-          loadingText="Verifying…"
-        />
-        <LoaderButton onClick={this.handleClick}
-          block
-          bsSize="large"
-          disabled={false}
-          type="submit"
-          isLoading={this.state.isLoading2}
-          text="Resend Code"
-          loadingText="Resending..."
-        />
-      </form>
-    );
-  }
+confirm(user, confirmationCode) {
+  return new Promise((resolve, reject) =>
+  user.confirmRegistration(confirmationCode, true, function(err, result) {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(result);
+  })
+);
+}
 
-  renderForm() {
-    return (
-      <div id='webpage' style={{width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '0px'}}>
+authenticate(user, username, password) {
+  const authenticationData = {
+    Username: username,
+    Password: password
+  };
+  const authenticationDetails = new AuthenticationDetails(authenticationData);
 
-        <div id='logoSide' style={{display : 'inline-block', width: '50%', backgroundColor: "#711AAC80", float: 'top', padding: '0px'}}>
-          <br></br><br></br>
-          <br></br><br></br>
+  return new Promise((resolve, reject) =>
+  user.authenticateUser(authenticationDetails, {
+    onSuccess: result => resolve(),
+    onFailure: err => reject(err)
+  })
+);
+}
 
-          <img src={logo} width="80%" height="80%" />
+resendConfirmCode() {
+  return new Promise((resolve, reject) =>
+  this.state.newUser.resendConfirmationCode(function(err, result) {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(result);
+  })
+);
+}
 
-          <br></br><br></br>
-          <br></br><br></br>
+renderConfirmationForm() {
+  return (
+    <form onSubmit={this.handleConfirmationSubmit}>
+    <FormGroup controlId="confirmationCode" bsSize="large">
+    <ControlLabel>Confirmation Code</ControlLabel>
+    <FormControl
+    autoFocus
+    type="tel"
+    value={this.state.confirmationCode}
+    onChange={this.handleChange}
+    />
+    <HelpBlock>Please check your email for the code.</HelpBlock>
+    </FormGroup>
+    <LoaderButton
+    block
+    bsSize="large"
+    disabled={!this.validateConfirmationForm()}
+    type="submit"
+    isLoading={this.state.isLoading}
+    text="Verify"
+    loadingText="Verifying…"
+    />
+    <LoaderButton onClick={this.handleClick}
+    block
+    bsSize="large"
+    disabled={false}
+    type="submit"
+    isLoading={this.state.isLoading2}
+    text="Resend Code"
+    loadingText="Resending..."
+    />
+    </form>
+  );
+}
+
+renderForm() {
+  return (
+
+    <div id='webpage' style={{width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center', padding: '0px', backgroundColor: 'white', position: 'absolute', top: '0', overflowX: 'hidden'}}>
 
 
-        </div>
+    <div class="row" style={{height: '100%'}}>
+    <div class="col-md-6" style={{backgroundColor: 'white', height: '100%'}}>
+    <div style={{width: '320px', margin: '100px auto 25px auto'}}>
+    <div style={{fontSize: '1.688em', fontWeight: '700', lineHeight: '95%'}}>Start finding influencers right now</div>
+    <div style={{fontSize:'1.125em', fontWeight: '700', marginTop: '25px'}}>Join Sapie Today!</div>
+    </div>
 
-        <div id="form" style={{display : 'inline-block', width: '50%', float: 'middle', height: '50%', padding: '0px'}}>
-          <center><h2><b> Join Sapie Today!</b></h2></center>
+    <form onSubmit={this.handleSubmit} style={{}}>
+    <FormGroup controlId="username" bsSize="large">
+    <FormControl
+    autoFocus
+    type="username"
+    value={this.state.username}
+    onChange={this.handleChange}
+    placeholder="Username"
+    />
+    </FormGroup>
+    <FormGroup controlId="email" bsSize="large">
+    <FormControl
+    autoFocus
+    type="email"
+    value={this.state.email}
+    onChange={this.handleChange}
+    placeholder="Email"
+    />
+    </FormGroup>
+    <FormGroup controlId="password" bsSize="large">
+    <FormControl
+    value={this.state.password}
+    onChange={this.handleChange}
+    type="password"
+    placeholder="Password"
+    />
+    </FormGroup>
+    <FormGroup controlId="confirmPassword" bsSize="large">
+    <FormControl
+    value={this.state.confirmPassword}
+    onChange={this.handleChange}
+    type="password"
+    placeholder= "Confirm password"
+    />
+    </FormGroup>
+    <p style={{color: 'rgba(0,0,0,.5)', fontSize: '.9em', marginBottom: '30px'}}>Password must be 8 characters long, with one special character, and one number *</p>
+    <LoaderButton
+    block
+    bsSize="large"
+    style={submitButtonStyle}
+    disabled={!this.validateForm()}
+    type="submit"
+    isLoading={this.state.isLoading}
+    text="Get Started"
+    loadingText="Signing up…"
+    />
+    </form>
+    <div style={{width: '320px', margin: '10px auto 0px auto'}}>
+    <div style={{fontSize:'1.125em', fontWeight: '400', marginTop: '25px'}}>Already a Member? <Link to ={'/app/login'}>Sign in</Link></div>
+    </div>
 
-          <br></br> <br></br>
-          <form onSubmit={this.handleSubmit} style={{}}>
-            <FormGroup controlId="username" bsSize="large">
-              <ControlLabel>Username</ControlLabel>
-              <FormControl
-                autoFocus
-                type="username"
-                value={this.state.username}
-                onChange={this.handleChange}
-                placeholder="username"
-              />
-            </FormGroup>
-            <FormGroup controlId="email" bsSize="large">
-              <ControlLabel>Email</ControlLabel>
-              <FormControl
-                autoFocus
-                type="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-                placeholder="test@example.com"
-              />
-            </FormGroup>
-            <FormGroup controlId="password" bsSize="large">
-              <ControlLabel>Password</ControlLabel>
-              <FormControl
-                value={this.state.password}
-                onChange={this.handleChange}
-                type="password"
-                placeholder="A-Z + a-z + 0-9 + !@#$%^"
-              />
-            </FormGroup>
-            <FormGroup controlId="confirmPassword" bsSize="large">
-              <ControlLabel>Confirm Password</ControlLabel>
-              <FormControl
-                value={this.state.confirmPassword}
-                onChange={this.handleChange}
-                type="password"
-                placeholder= "confirm password"
-              />
-            </FormGroup>
-            <LoaderButton
-              block
-              bsSize="large"
-              style={submitButtonStyle}
-              disabled={!this.validateForm()}
-              type="submit"
-              isLoading={this.state.isLoading}
-              text="Get Started"
-              loadingText="Signing up…"
-            />
-          </form>
-          <br></br><br></br>
-          <center><h3><b> Already a Member?</b></h3>
-          <Link to ={'/app/login'}>Sign in</Link>
-          </center>
 
-        </div>
 
-      </div>
-    );
-  }
+    </div>
 
-  render() {
-    return (
-      <div className="Signup" style={{padding: '0px'}}>
-        {this.state.newUser === null
-          ? this.renderForm()
-          : this.renderConfirmationForm()}
+    <div class="col-md-6" style={{backgroundColor: '#a269c8', height: '100%', overflow: 'hidden'}}>
+    <div style={{position: 'absolute', textAlign: 'left', width: '60%', color: 'white', fontSize: '1.325em', zIndex: '2', top: '30%', left: '20%', fontWeight: '700'}}>
+    <div><img src={searchIcon} style={{height: '20px', marginRight: '10px'}} />Search for influencers.</div>
+
+    <div style={{marginTop: '48px', marginBottom: '48px'}}><img src={multipleIcon} style={{height: '20px', marginRight: '10px'}} />Find real influencers. </div>
+    <div><img src={connectionsIcon} style={{height: '20px', marginRight: '10px'}} />Make connections.</div>
+    </div>
+    <img src={sapielogo} style={{height: '100%', position: 'absolute', left: '50%', zIndex: '1'}} />
+    </div>
+    </div>
+
+
+    </div>
+  );
+}
+
+render() {
+  return (
+    <div className="Signup" style={{padding: '0px'}}>
+    {this.state.newUser === null
+      ? this.renderForm()
+      : this.renderConfirmationForm()}
       </div>
     );
   }
