4ff688ca3b518e64cefa1f8bfe189dd599b4e08c
phara23 <instaparktechnologies@gmail.com>
2018-03-31 21:11:00 -0700

    Update influencer.py

--- backend/influencer.py
+++ backend/influencer.py
@@ -66,22 +66,15 @@ class Influencer:
     def query(cls, query, limit=100):
         """Query for a list of influencers"""
         if isinstance(query, str):
-            #actual_query = dict(
-            #    size=10000,
-            #    sort=["influencer_score"],
-            #    query=dict(
-            #        query_string=dict(
-            #            query=query,
-            #        ),
-            #    ),
-            #)
-            actual_query = {
-              "query": {
-                "match_phrase": {
-                  "youtube": query
-                }
-              }
-            }
+            actual_query = dict(
+                size=10000,
+                sort=["influencer_score"],
+                query=dict(
+                    query_string=dict(
+                        query=query,
+                    ),
+                ),
+            )
         elif query is None:
             actual_query = MATCH_ALL
         else:
@@ -158,22 +151,15 @@ class Influencer:
                             synonyms.append(l.name())
                 
                 for ss in synonyms:
-                    #actual_query = dict(
-                    #    size=10000,
-                    #    sort=["influencer_score"],
-                    #    query=dict(
-                    #        query_string=dict(
-                    #            query=ss,
-                    #        ),
-                    #    ),
-                    #)
-                    actual_query = {
-                      "query": {
-                        "match_phrase": {
-                          "youtube": ss
-                        }
-                      }
-                    }
+                    actual_query = dict(
+                        size=10000,
+                        sort=["influencer_score"],
+                        query=dict(
+                            query_string=dict(
+                                query=ss,
+                            ),
+                        ),
+                    )
 
                     res = es.search(
                         index=cls.index,
