82b67005bec606b186acc2a2fcbd0df9d3e5c055
Nick Bavafa <nbavafa@gmail.com>
2018-05-07 22:51:26 -0700

    Bug fixes

--- frontend/src/page/components/CheckboxOrRadioGroup.js
+++ frontend/src/page/components/CheckboxOrRadioGroup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class CheckboxOrRadioGroup extends {
+class CheckboxOrRadioGroup extends React.Component {
 
   render() {
     return (
