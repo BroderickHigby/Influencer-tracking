5a2c8581029880a711ef3d4229ac4b0403a7e715
Nick Bavafa <nbavafa@gmail.com>
2018-05-07 22:49:27 -0700

    Bug fixes

--- frontend/src/page/components/CheckboxOrRadioGroup.js
+++ frontend/src/page/components/CheckboxOrRadioGroup.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CheckboxOrRadioGroup = (props) => (
-  <div>
-    <label className="form-label">{props.title}</label>
-    <div className="checkbox-group">
-      {this.props.options.map(opt => {
-        return (
-          <label key={opt} className="form-label capitalize">
-            <input
-              className="form-checkbox"
-              name={this.props.setName}
-              onChange={this.props.controlFunc}
-              value={opt}
-              checked={ this.props.selectedOptions.indexOf(opt) > -1 }
-              type={this.props.type} /> {opt}
-          </label>
-        );
-      })}
-    </div>
-  </div>
-);
+class CheckboxOrRadioGroup extends {
+
+  render() {
+    return (
+      <div>
+        <label className="form-label">{props.title}</label>
+        <div className="checkbox-group">
+          {this.props.options.map(opt => {
+              <label key={opt} className="form-label capitalize">
+                <input
+                  className="form-checkbox"
+                  name={this.props.setName}
+                  onChange={this.props.controlFunc}
+                  value={opt}
+                  checked={ this.props.selectedOptions.indexOf(opt) > -1 }
+                  type={this.props.type} /> {opt}
+              </label>
+          })}
+        </div>
+      </div>
+    );
+  }
+
+}
 
 CheckboxOrRadioGroup.propTypes = {
   title: PropTypes.string.isRequired,
