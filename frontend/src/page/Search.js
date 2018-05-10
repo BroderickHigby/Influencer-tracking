3b984911cf64ff8aa6ebf920787e53abc437a106
Nick Bavafa <nbavafa@gmail.com>
2018-05-10 14:05:20 -0700

    Potential fix to adding twitter and insta

--- frontend/src/page/Search.js
+++ frontend/src/page/Search.js
@@ -1182,7 +1182,7 @@ class Search extends Component {
                   }
                   {
                     (d.youtube.brandingSettings.channel) ? (
-                      (d.youtube.brandingSettings.channel.keywords) (
+                      (d.youtube.brandingSettings.channel.keywords) ? (
                         <p style={accountsStyle}>Keywords: {addCommas(truncation(d.youtube.brandingSettings.channel.keywords,65))} </p>
                       ) : ( "" )
                     ) : ( "" )
