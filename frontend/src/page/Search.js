547fe21de282ecabc4d92d672d5106a6a969aeb2
Nick Bavafa <nbavafa@gmail.com>
2018-05-11 03:48:39 -0700

    Removed FE part

--- frontend/src/page/Search.js
+++ frontend/src/page/Search.js
@@ -772,6 +772,8 @@ class Search extends Component {
           <br />
           </center>
           <br />
+          {/*
+
           <div style = {{fontColor: darkColor, margin: '2px'}}>
           <center>
           <form>
@@ -787,6 +789,8 @@ class Search extends Component {
           </center>
           </div>
           <br />
+
+          */}
 
             <p style={{color: darkColor, paddingLeft: '10px'}}> We found... </p>
             <div style={{color: 'rgba(0,0,0,0.5)', fontSize: '1em', padding: '3px', marginLeft: '20px'}}>
